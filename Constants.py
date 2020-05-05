0f67795c2d1ba4fffa809410ed423d5874607ddf
Jien Li <jienli@Jiens-MacBook-Pro.local>
2020-05-04 23:06:24 -0400

    pics

--- Constants.py
+++ Constants.py
@@ -1,5 +1,3 @@
-
-    # partial minmax -- ok , entire hisotry minmax -- really good
 TEST = 100
 REP = 200
 INFO = 10
@@ -12,6 +10,39 @@ TICKER = 'GOOG'
 
 BIAS = -1
 DRUNKNESS_INDEX = 1.2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#     # partial minmax -- ok , entire hisotry minmax -- really good
+# TEST = 100
+# REP = 200
+# INFO = 10
+# STARTDATE = 2500
+# TRAIN_ITERATION = 10000
+# NUM_OF_STARTING_POINTS = 5
+# STRUCTURE = [10,3,1]
+# TICKER = 'GOOG'
+
+
+# BIAS = -1
+# DRUNKNESS_INDEX = 1.2
 
 #     # partial minmax -- shitty , entire hisotry minmax -- really good
 # TEST = 100
